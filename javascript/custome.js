97487f3c509270266caa58f77fadde650b220ec7
mehedidev1 <201hasanmehedi@gmail.com>
2023-10-01 12:31:23 +0600

    first commit

---
+++ javascript/custome.js
@@ -0,0 +1,24 @@
+
+/*
+// input method start
+
+// for 7 number button 
+document.getElementById('seven').addEventListener('click',function(){
+    const inputValue=document.getElementById('seven').value;
+    document.getElementById('display_input').value=document.getElementById('display_input').value+inputValue;
+})
+//for 8 number button
+
+document.getElementById('eight').addEventListener('click',function(){
+    const inputValue=document.getElementById('eight').value;
+    document.getElementById('display_input').value= document.getElementById('display_input').value+inputValue;
+    
+})
+
+
+// input method end
+*/
+
+function calculator(x){
+    form.display.value=form.display.value+x;
+}
